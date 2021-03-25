e04441ccef3477587333fb4ee2029916dd62f1c0
Henri <henri.lieutaud@gmail.com>
2021-03-25 22:26:08 +0100

    Initial commit

---
+++ contracts/ToutDoucement.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Metadata.sol"; 
+
+contract ToutDoucement is IERC721Metadata, ERC721 {
+	uint256 public tokenCounter;
+  constructor() ERC721("Victoire Oberkampf - Tout Doucement", "TOUDOU") public {
+  	_mint(msg.sender, tokenCounter);
+    tokenCounter++;
+    }
+
+  function tokenURI(uint256 tokenId) public view virtual override (ERC721, IERC721Metadata) returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory _tokenURI = "http://www.victoire-oberkampf.fr/NFT-Workshop/ToutDoucement.json";
+        return _tokenURI;
+          }
+
+   function buyAToken() public payable
+   {
+   	require(msg.value > 100000000000000000);
+   	_mint(msg.sender, tokenCounter);
+   	tokenCounter++;
+
+   }
+}
---
+++ migrations/2_deploy.js
@@ -0,0 +1,7 @@
+const SongForACity = artifacts.require("SongForACity");
+const ToutDoucement = artifacts.require("ToutDoucement");
+
+module.exports = function (deployer) {
+  deployer.deploy(SongForACity);
+  deployer.deploy(ToutDoucement);
+};
